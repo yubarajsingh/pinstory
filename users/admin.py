0b7b91dfd7ae20f8a2d401a7029459f652da5f4b
yubaraj singh <36361468+yubarajsingh@users.noreply.github.com>
2022-01-20 21:29:48 +0545

    Add files via upload

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import User,Category,Story,StoryIten
 
-admin.site.register(User)
+
 admin.site.register(Category)
 admin.site.register(StoryIten)
 admin.site.register(Story)
